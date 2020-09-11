6a8202edf9812ce764611248bd6f8ecea11563b2
Kevin Glasgow <kglas001@gmail.com>
2020-09-11 18:59:49 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -13,7 +13,7 @@ def assembled_aoh
     { name:  "Don Gately", occupation: "Live-in Staff" },
     { name:  "Joelle van Dyne", occupation: "Radio Personality" },
     { name:  "Kate Gompert", occupation: "None" },
-    
+    { name:  "Bruce Green", occupation: "Fan of Mildred" }
     ]
 end
 
